65e5d359836296ed667f793da4ca0c014912c372
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-01-22 09:13:01 +0200

    feat: add about section

---
+++ src/components/About.vue
@@ -0,0 +1,119 @@
+<template>
+  <section class="about section">
+    <div class="about__container">
+      <h2 class="about__title">{{ title }}</h2>
+      <div class="about__content">
+        <div class="about__text">
+          <h3 class="about__subtitle">{{ subtitle }}</h3>
+          <p class="about__description">{{ description }}</p>
+        </div>
+        <div class="about__image-wrapper">
+          <img class="about__image" src="/images/profile.png" alt="profile">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.about {
+  padding: 60px;
+  background: #002ed0;
+  color: #fff;
+}
+
+.about__container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.about__title {
+  font-size: 110px;
+  color: #fff;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: #fff;
+  margin-bottom: 32px;
+}
+
+.about__content {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 700px;
+}
+
+.about__text {
+  padding-right: 20px;
+}
+
+.about__subtitle {
+  font-size: 48px;
+  line-height: 1.5;
+  margin-bottom: 32px;
+}
+
+.about__description {
+  line-height: 2.4;
+}
+
+.about__image-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+.about__image {
+  width: 420px;
+  height: 610px;
+}
+
+@media screen and (max-width: 1000px) {
+  .about {
+    padding: 30px;
+  }
+
+  .about__content {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+  }
+
+  .about__image-wrapper {
+    margin-top: 40px;
+  }
+
+  .about__title {
+    font-size: 70px;
+  }
+}
+
+@media screen and (max-width: 468px) {
+  .about__subtitle {
+    font-size: 32px;
+  }
+
+  .about__image {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <Hero v-bind="hero" />
+    <About v-bind="about" />
   </div>
 </template>
 
 <script>
 import Hero from '@/components/Hero.vue';
+import About from '@/components/About.vue';
 
 export default {
   name: 'Home',
@@ -16,10 +18,19 @@ export default {
         title: 'User Experience Designer',
         description: 'I have years of experiance working with small and large companies and I\'m currently looking for the new opportunity.',
       },
+      about: {
+        title: 'Hello, I\'m John',
+        subtitle: 'I am John Doe, a UI/UX Consultant currently living in San Franciso, CA.',
+        description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores,
+          nesciunt necessitatibus nobis nisi voluptate suscipit, quaerat officia
+          unde obcaecati non aut quod. Nihil commodi eius nesciunt assumenda impedit
+          doloremque repudiandae!`,
+      },
     };
   },
   components: {
     Hero,
+    About,
   },
 };
 </script>
